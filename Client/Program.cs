f326247b2c6b9c7af1fbbe17f389d0405ac1a7e6
Alexandros Mamouzelos <almamouz@gmail.com>
2013-04-02 08:00:09 +0200

    Added client with timer that repeats tasks every 15 seconds

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Timers;
+
+namespace Client
+{
+    class Program
+    {
+        static string baseAddress = "http://localhost:1412/";
+        static HttpClient client = new HttpClient() { BaseAddress = new Uri(baseAddress) };
+        static int interval = 15000;
+
+        public static void Repeater()
+        {
+            Console.WriteLine("Client received: {0}", client.GetStringAsync("api/energy").Result);
+            Console.WriteLine();
+        }
+        
+        static void Main(string[] args)
+        {
+            var timer = new Timer(interval);
+            timer.Elapsed += (s, e) => Repeater();
+            timer.Start();
+            Console.ReadLine();
+        }
+    }
+}
--- RESTService/Global.asax.cs
+++ RESTService/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RESTService.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -13,7 +15,7 @@ namespace RESTService
     // visit http://go.microsoft.com/?LinkId=9394801
 
     public class WebApiApplication : System.Web.HttpApplication
-    {
+    {        
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
